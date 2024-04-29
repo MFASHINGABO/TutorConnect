25b43cbd71e6d3680f9918ffbbeb39ba9ade1b9d
mfashingabo <mfashprince@gmail.com>
2024-04-29 18:13:37 +0200

    startUp project

---
+++ src/main/java/com/TutorCon/TutorConnet/TutorConnetApplication.java
@@ -0,0 +1,13 @@
+package com.TutorCon.TutorConnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorConnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorConnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TutorCon/TutorConnet/TutorConnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TutorCon.TutorConnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorConnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
